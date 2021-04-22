56b036525edf1c0f01fc14add0b5f9801febaa59
Shruthika-afk <75931321+Shruthika-afk@users.noreply.github.com>
2021-04-22 10:13:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,26 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide()
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX =" + leftWristX + "leftWristY =" + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;        
+        console.log("rightWristX =" + rightWristX + "rightWristY =" + rightWristY);
+    }
+}
+
+function modelLoaded() {
+    console.log("PoseNet is Initialized");
 }
 
 function preload() {
@@ -14,6 +34,12 @@ function preload() {
 function play() {
     song1.play();
     song2.play();
+
+    song1.setVolume(1);
+    song1.rate(1);
+
+    song2.setVolume(1);
+    song2.rate(1);
 }
 
 function draw() {
