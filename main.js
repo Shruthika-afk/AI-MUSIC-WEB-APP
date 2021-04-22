b2c67f4b8114d54f67b3b98e07aae134daddb023
Shruthika-afk <75931321+Shruthika-afk@users.noreply.github.com>
2021-04-22 10:09:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ function setup() {
     video = createCapture(VIDEO);
     video.hide()
 }
+
+function preload() {
+    song1 = loadSound("faded.mp3");
+    song2 = loadSound("lovely.mp3");
+}
+
+function play() {
+    song1.play();
+    song2.play();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
